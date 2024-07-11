511943a04fb9a46ca0de43434385b169604608f2
srikanthyaswada <srikanthyaswada.vita@gmail.com>
2024-07-11 17:34:57 +0530

    get method

--- controllers/vitaControllers.js
+++ controllers/vitaControllers.js
@@ -1,15 +1,34 @@
-const adminModel = require('../controllers/vitaControllers')
-
+const adminModel = require("../models/vitaModel");
 
 exports.register = async (req, res) => {
-    try {
-      const user = new adminModel(req.body);
-      await user.save();
-      res.status(201).json(user);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Failed to add admin" });
-    }
-  };
+  try {
+    const user = new adminModel(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to add admin" });
+  }
+};
 
-  
+exports.getUsers = async (req, res) => {
+  try {
+    const userlist = await adminModel.find();
+    res.status(201).json(userlist);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "failed to get user data" });
+  }
+};
+exports.getProfile = async (req, res) => {
+  try {
+    const profile = await adminModel.findById(req.params.id);
+    if (!profile) {
+      return res.status(404).json({ error: "user not found" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch user profile" });
+  }
+};
--- routes/vitaRoute.js
+++ routes/vitaRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/vitaControllers')
+const controller = require("../controllers/vitaControllers");
 
 router.post("/adduser", controller.register);
+router.get("/getusers", controller.getUsers);
+router.get("/profile/:id", controller.getProfile)
 
 module.exports = router;
